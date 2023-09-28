86082ebb02f0223daeac368ac99a50768514a5ae
Allan Pereira <allangsp92@gmail.com>
2023-09-28 11:56:56 -0300

    Inicio

---
+++ main.py
@@ -0,0 +1,5 @@
+#Este é um teste para a disciplina de Fundamentos de Design de Sistemas
+#Neste pequeno projeto será aplicado um exercicio
+#Trata-se de uma calculadora que converte Graus Fahrenheit para Celsius
+
+#Por Allan Pereira, 28 de setembro de 2023
